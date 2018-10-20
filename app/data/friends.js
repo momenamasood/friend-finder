e04cba33f82bec15801a7cd38e610cd33b1bb6fb
Momena07 <momena.masood@gmail.com>
2018-10-20 11:48:45 -0400

    Picture upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,8 @@
 //initialize starting with 'dummy' data
 var friendList = [
     {
-      name: 'Momena Masood',
-      profilePic: '',
+      name: 'Beth',
+      profilePic: 'https://www.google.ca/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiDmO-Nr5XeAhUJSN8KHW-mC2cQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fgirl%2F&psig=AOvVaw0hp52sDTtLuXsq-3kuE9FA&ust=1540136874704387',
       scores: [
         "4",
         "5",
